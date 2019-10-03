785b8b4ed1cb744b289835bc39d22e045bef8b72
Singon <singond@seznam.cz>
2019-10-03 18:07:53 +0200

    Make caesar apply only to valid characters

--- src/octave/caesar.m
+++ src/octave/caesar.m
@@ -1,3 +1,8 @@
 function C = caesar(str, rot)
+	ab = classcrypt_settings.alphabet;
+	C = strfun(@caesar_impl, str, ab, rot);
+endfunction
+
+function C = caesar_impl(str, rot)
 	C = idx2str(str2idx(str) + rot);
 endfunction
--- src/octave/strfun.m
+++ src/octave/strfun.m
@@ -1,7 +1,13 @@
-function R = strfun(func, str)
+## Apply a function to a specified subset of characters in a string.
+function R = strfun(func, str, chars, varargin)
 	R = str;
-	ab = classcrypt_settings.alphabet;
-	M = ismember(str, ab);
-#	R(M) = arrayfun(func, str(M));
-	R(M) = func(str(M));
+	if (nargin < 2)
+		print_usage;
+		return;
+	elseif (nargin < 3)
+		M = true(size(R));
+	else
+		M = ismember(str, chars);
+	endif
+	R(M) = func(str(M), varargin{:});
 endfunction
